f0ad74facb3ea89cf433fbcd47d6c55efabf17cf
pigglehyun <sarah0661@pusan.ac.kr>
2025-10-02 15:13:06 +0900

    Feat: Token Provider

--- src/main/java/com/dayeyak/queue/token/TokenProvider.java
+++ src/main/java/com/dayeyak/queue/token/TokenProvider.java
@@ -1,11 +1,10 @@
 package com.dayeyak.queue.token;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.security.Key;
 import java.util.Base64;
@@ -13,25 +12,27 @@ import java.util.Date;
 import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class TokenProvider {
 
-    @Value("${jwt.secret}")
-    private String secretKey ;
-    private final long waitingTokenExpirationTime = 9000000;
-    private final long activeTokenExpirationTime = 9000000;
+    //    @Value("${jwt.secret}")
+    private String secretKey = "124saxd123423ajsdfhkaishfiukweyhksefhskheskufhsekufhsekfhsukefsefsdfkj32j45k214jkjskj124124";
+    private final long waitingTokenExpirationTime = 10 * 60 * 1000L; //1시간
 
+    @Transactional
     public String createWaitingToken(String userId) {
         Date now = new Date();
         Date expireAt = new Date(now.getTime() + waitingTokenExpirationTime);
-
         return Jwts.builder()
                 .setSubject(userId)
                 .setIssuedAt(now)
+                .setId(UUID.randomUUID().toString())       // 토큰 고유 ID
                 .setExpiration(expireAt)
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
+    @Transactional
     public UUID createActiveToken(String userId, String waitingToken) {
 
         //유저 확인
@@ -40,21 +41,33 @@ public class TokenProvider {
         }
         //waiting token 만료되었는지 확인
         if (isExpired(waitingToken)) {
+            return null;
             //만료됐으면 버리기
-            throw new RuntimeException("토큰 만료됨. 페이지 나간 유저");
         }
         //만료 안됐으면 token 기반으로 새로운 token 생성
         UUID uuid = UUID.nameUUIDFromBytes(waitingToken.getBytes());
+
         return uuid;
     }
 
+    @Transactional
     public String getUserIdFromWaitingToken(String token) {
-        return Jwts.parserBuilder()
+        Claims claims = Jwts.parserBuilder()
                 .setSigningKey(getSigningKey())
                 .build()
                 .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+                .getBody();
+        return claims.getSubject();
+//        return Jwts.parserBuilder()
+//                .setSigningKey(getSigningKey())
+//                .build()
+//                .parseClaimsJws(token)
+//                .getBody()
+//                .getSubject();
+    }
+
+    public boolean isValid(String oldToken) {
+        return true;
     }
 
     private boolean isExpired(String token) {
